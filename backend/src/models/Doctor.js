8215ef1212307f8f189f148ebc21446db44a2dc9
bodilypat <139784676+bodilypat@users.noreply.github.com>
2026-02-06 00:13:41 -0800

    Add files via upload

---
+++ backend/src/models/Doctor.js
@@ -0,0 +1,34 @@
+//src/models/Doctor.js 
+
+const mongoose = require('mongoose');
+
+const doctorSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    specialization: {
+        type: String,
+        required: true
+    },
+    experience: {
+        type: Number,
+        required: true
+    },
+    contact: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    avaiable: {
+        type: Boolean,
+        default: true
+    }
+});
+
+module.exports = mongoose.model('Doctor', doctorSchema);
+
